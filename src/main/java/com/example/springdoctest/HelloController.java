ce601429561790639d6a5a528fc5c2047c6bf4fa
Biluohen <xx@dev.com>
2022-09-05 17:38:08 +0800

    add example

--- src/main/java/com/example/springdoctest/HelloController.java
+++ src/main/java/com/example/springdoctest/HelloController.java
@@ -5,6 +5,8 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @Validated
 public class HelloController {
@@ -16,6 +18,11 @@ public class HelloController {
 
     @GetMapping("hello/param_object")
     public String helloObject(@Validated @ParameterObject HelloReq req) {
+        return "obj";
+    }
+
+    @GetMapping("hello/array_enum")
+    public String helloObject(List<HelloEnum> option) {
         return "obj";
     }
 }
